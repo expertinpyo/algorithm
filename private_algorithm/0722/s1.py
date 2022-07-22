ffb24b6597ba63f52bf83a4b340443721457ec3f
expertinpyo <ypd02363@naver.com>
2022-07-22 22:08:53 +0900

    0722 algo

---
+++ private_algorithm/0722/s1.py
@@ -0,0 +1,16 @@
+# 17425
+def isTrue(x):
+    for i in range()
+
+T = int(input())
+arr = [1] * (10 ** 6 + 1)
+for i in range(2, int(10**6**0.5)+1):
+    if arr[i]:
+        for j in range(2*i, 10**6+1, i):
+            arr[j] = 0
+for i in range(2, 10**6+1):
+
+
+for _ in range(T):
+    n = int(input())
+    isTrue(n)
