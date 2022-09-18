2e78d2368c4a664cd935267ed09993a501c87758
expertinpyo <97691381+expertinpyo@users.noreply.github.com>
2022-09-18 23:31:11 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 2256 ms, Memory: 38892 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,12 +1,16 @@
-nums = [True] * 1000001
-for i in range(2, 1001):
-    if nums[i]:
-        for j in range(2*i, 1000001, i):
-            nums[j] = False
+num = []
 while True:
     n = int(input())
     if not n:
         break
+    num.append(n)
+n = max(num)
+nums = [True] * (n+1)
+for i in range(2, int(n**0.5)+1):
+    if nums[i]:
+        for j in range(2*i, n+1, i):
+            nums[j] = False
+for n in num:
     for i in range(3, n, 2):
         if nums[i] and nums[n-i]:
             print(f"{n} = {i} + {n-i}")
