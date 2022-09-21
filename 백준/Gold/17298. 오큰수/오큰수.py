079fa5aa8daa8fdfb2a5f368c5afffe8795526ba
expertinpyo <97691381+expertinpyo@users.noreply.github.com>
2022-09-21 20:36:31 +0900

    [Gold IV] Title: 오큰수, Time: 1228 ms, Memory: 149436 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,13 +1,12 @@
-# 17298 based on stack
-import sys
-input = sys.stdin.readline
 N = int(input())
-arr = list(map(int, input().split()))
-stack = [0]
+arr = list(map(int,input().split()))
+stack = [arr[-1]]
 ans = [-1] * N
-
-for i in range(1, N):
-    while stack and arr[stack[-1]] < arr[i]:
-        ans[stack.pop()] = arr[i]
-    stack.append(i)
+for i in range(N-2, -1, -1):
+    while stack:
+        if arr[i] < stack[-1]:
+            ans[i] = stack[-1]
+            break
+        stack.pop()
+    stack.append(arr[i])
 print(*ans)
