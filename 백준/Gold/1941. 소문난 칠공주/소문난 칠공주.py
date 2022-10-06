46f4519e2b0c10f40f6429f161acb69b7d791f86
expertinpyo <97691381+expertinpyo@users.noreply.github.com>
2022-10-06 22:37:36 +0900

    [Gold III] Title: 소문난 칠공주, Time: 2000 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,36 +1,29 @@
 from itertools import combinations as cb
 from collections import deque
 
+def bfs(c):
+    queue = deque([c.pop()])
+    while queue:
+        x, y = queue.popleft()
+        for d in di:
+            nx, ny = d[0] + x, d[1] + y
+            if (nx, ny) in c:
+                c.remove((nx, ny))
+                queue += (nx, ny),
+    return not c
+
 arr = [list(input()) for _ in range(5)]
-cbs = list(cb(range(25), 7))
+cbs = cb(set((i, j) for i in range(5) for j in range(5)), 7)
 di = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 ans = 0
 for c in cbs:
     cnt_y = 0
-    fail = False
-    for i in c:
-        a, b = i // 5, i % 5
-        if arr[a][b] == 'Y':
+    for i, j in c:
+        if arr[i][j] == 'Y':
             cnt_y += 1
         if cnt_y > 3:
-            fail = True
             break
-    if not fail:
-        visited = [[0] * 5 for _ in range(5)]
-        for i in c:
-            a, b = i // 5, i % 5
-            visited[a][b] = 1
-        queue = deque([(a, b)])
-        check = 1
-        visited[a][b] = 0
-        while queue:
-            x, y = queue.popleft()
-            for d in di:
-                nx, ny = d[0] + x, d[1] + y
-                if 0 <= nx < 5 and 0 <= ny < 5 and visited[nx][ny]:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = 0
-                    check += 1
-        if check == 7:
-            ans += 1
+    if cnt_y > 3:
+        continue
+    ans += bfs(set(c))
 print(ans)
