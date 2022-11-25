06946ea4815fab6ab696f7d8c909db05d64c558a
expertinpyo <97691381+expertinpyo@users.noreply.github.com>
2022-11-25 13:56:21 +0900

    [Gold IV] Title: 테트로미노, Time: 320 ms, Memory: 37316 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,29 +1,22 @@
-from collections import deque
-from copy import deepcopy
-def bfs(x, y):
+def dfs(x, y, depth, amt):
     global ans
-    queue = deque([(x, y, 1, arr[x][y], [(x, y)])])
+    if depth == 4:
+        ans = max(amt, ans)
+        return
+    if (4-depth) * max_n + amt < ans:
+        return
 
-    while queue:
-        x, y, length, amt, route = queue.popleft()
-        if length == 4:
-            ans = max(ans, amt)
-        if (4-length) * max_n + amt < ans:
-            continue
-        for d in di:
-            nx, ny = x + d[0], y + d[1]
-            if 0 <= nx < N and 0 <= ny < M and (nx, ny) not in route:
-                new_route = deepcopy(route)
-                new_route.append((nx, ny))
-                queue.append((nx, ny, length + 1, amt + arr[nx][ny],new_route ))
-                if length <= 2:
-                    for d2 in di:
-                        nnx, nny = x + d2[0], y + d2[1]
-                        if 0 <= nnx < N and 0 <= nny < M and (nnx, nny) not in new_route:
-                            new_route2 = deepcopy(new_route)
-                            new_route2.append((nnx, nny))
-                            queue.append((nnx, nny, length + 2, amt + arr[nx][ny] + arr[nnx][nny], new_route2))
+    for d in di:
+        nx, ny = x + d[0], y + d[1]
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+            if depth <= 2:
+                visited[nx][ny] = 1
+                dfs(x, y, depth + 1, amt + arr[nx][ny])
+                visited[nx][ny] = 0
 
+            visited[nx][ny] = 1
+            dfs(nx, ny, depth + 1, amt + arr[nx][ny])
+            visited[nx][ny] = 0
 
 di =[[0, 1], [1, 0], [-1, 0], [0, -1]]
 N, M = map(int, input().split())
@@ -36,10 +29,9 @@ max_n = 0
 for i in range(N):
     max_n = max(max_n, max(arr[i]))
 
-
 for i in range(N):
     for j in range(M):
-        if not visited[i][j]:
-            bfs(i, j)
-            visited[i][j] = 1
+        visited[i][j] = 1
+        dfs(i, j, 1, arr[i][j])
+        visited[i][j] = 0
 print(ans)
