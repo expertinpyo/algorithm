fb0dbb4ac683159ddef2f32b1648c525e5bf11c0
expertinpyo <97691381+expertinpyo@users.noreply.github.com>
2022-11-25 15:09:57 +0900

    [Gold IV] Title: 연구소 3, Time: 1292 ms, Memory: 32524 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -4,7 +4,6 @@ from collections import deque
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 virus = []
-total = 0
 zero = 0
 di = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
@@ -15,7 +14,6 @@ for i in range(N):
                 virus.append((i, j))
             else:
                 zero += 1
-            total += 1
 
 if not zero:
     print(0)
@@ -28,23 +26,22 @@ for idx in idxs:
     visited = [[0] * N for _ in range(N)]
     for x, y in idx:
         queue.append((x, y, 0))
-        visited[x][y] = 1
+        visited[x][y] = 1   # 첫 시작 좌표 설정
 
     cnt = 0
     while queue:
         x, y, time = queue.popleft()
         cnt = max(time, cnt)
-        if cnt >= zero:
+        if cnt >= zero: # 더 점검 할 이유가 없음
             break
         for d in di:
             nx, ny = x + d[0], y + d[1]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and arr[nx][ny] != 1:
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and arr[nx][ny] != 1 and cnt < ans:
                 visited[nx][ny] = time + 1
                 queue.append((nx, ny, time + 1))
 
     visit = 0
     minus = True
-    # print(visited)
     for i in range(N):
         for j in range(N):
             if visited[i][j] and not arr[i][j]:
