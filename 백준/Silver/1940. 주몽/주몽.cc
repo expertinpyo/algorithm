effc2ec671a7f532ee84211b6df015ab984ea9b0
expertinpyo <97691381+expertinpyo@users.noreply.github.com>
2023-07-25 22:59:20 +0900

    [Silver IV] Title: 주몽, Time: 100 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -6,6 +6,10 @@ using namespace std;
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n, m;
 
 	cin >> n;
@@ -41,8 +45,6 @@ int main()
 			iter2++;
 			continue;
 		}
-		
-		
 		iter2++;
 	}
 
